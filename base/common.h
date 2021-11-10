81a8cdbbff4aa89a7b69e3ef7ed00f8ba6d53b08
mikemei <meixu@huawei.com>
2021-11-10 18:01:18 +0800

    Update to 6.2.0.300 version

--- base/common.h
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) Huawei Technologies Co., Ltd. 2019-2021. All rights reserved.
- * Description: common head file
- */
-
-#ifndef VULKANEXAMPLES_COMMON_H
-#define VULKANEXAMPLES_COMMON_H
-#define MY_DELETE(p)        { if (p) { delete (p);    (p) = nullptr; } }
-#endif // VULKANEXAMPLES_COMMON_H
